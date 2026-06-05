6447619a2839e9adc7e68499c87b3141e097cf05
Alekseev Oleg <67434901+alllekseev@users.noreply.github.com>
2026-06-05 20:42:11 +0300

    Initial Commit

---
+++ LearnMetal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearnMetal
+//
+//  Created by Oleg Alekseev on 05.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearnMetal/LearnMetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnMetalApp.swift
+//  LearnMetal
+//
+//  Created by Oleg Alekseev on 05.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct LearnMetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
